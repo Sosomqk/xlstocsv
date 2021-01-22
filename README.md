324267157a654b4032a11839a83c635d7d56c352
Miroslav Isikiyski <miroslav.isikiyski@linkmobility.com>
2021-01-22 17:18:54 +0200

    done

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-Patched working version of https://github.com/xevo/xls2csv
-
-The original version does not suport XLS. 
-This version converts xls to xlsx and then to csv.
+XLS to CSV converter.
+Note: it works only wit XLS files, for XLSX files use: https://github.com/xevo/xls2csv (xlsx2csv)
 
 Dependencies: pandas, xlrd, openpyxl
 
-Usage: `xlsx2csv /path/to/xls_or_xlsx_file/` # spits CSV lines on CLI
-Usage: `xlsx2csv /path/to/xls_or_xlsx_file/ /path/to/file.csv` # writes in a CSV file
+Usage: `xls2csv /path/to/xls_or_xlsx_file/` # spits CSV lines on CLI
+Usage: `xls2csv /path/to/xls_or_xlsx_file/ /path/to/file.csv` # writes in a CSV file
---
+++ xls2csv.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import csv
+import re
+import sys
+import pandas as pd
+
+if (len(sys.argv) < 2):
+	print('''Invalid file path!
+			Usage: xls2csv /path/to/xls_file  # for command line output
+			Usage: xls2csv /path/to/xls_file /path/to/csv_file  # for writing in a csv file
+	''')
+	sys.exit(1)
+
+argv_len = len(sys.argv)
+workbook = pd.read_excel(sys.argv[1], header=None, engine='xlrd') # only for xls
+
+if (argv_len > 2): # if output file is provided
+    csv_file = open(sys.argv[2], 'w')
+    writer = csv.writer(csv_file)
+
+for row in workbook.values:
+	if (argv_len > 2): # if output file is provided
+			writer.writerow(row)
+	else:
+			row2str = ','.join(str(s) for s in row)
+			row2str = re.sub(r"00\:00\:00|\s", '', row2str)
+			print(row2str)
+
+if (argv_len > 2): # if output file is provided
+    writer.close()
