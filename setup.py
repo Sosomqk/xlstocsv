da26554812c58e5edead9e5fcd8621ec4728e763
Miroslav Isikiyski <miroslav.isikiyski@linkmobility.com>
2021-01-22 22:37:26 +0200

    fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='xls2csv',
-    version='1.0.0',
+    version='1.0.1',
     description='XLS to CSV converter. Complementary to xlsx2csv package',
     long_description_content_type="text/markdown",
     long_description=README,
